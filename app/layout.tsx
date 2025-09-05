a5c32ec72cbdc55aa55bcc785c71ad3ada2a3d38
adspacheco <andersonac7@outlook.com>
2025-09-05 14:44:19 -0300

    refactor: convert `.js` files to `.tsx`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
-export const metadata = {
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
   title: "SaaS Boilerplate",
   description: "SaaS boilerplate inspired by TabNews.",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
