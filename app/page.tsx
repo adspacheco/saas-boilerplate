6a4c8fc5ca767e59c414609d2bb4c0720ba7f694
adspacheco <andersonac7@outlook.com>
2025-09-05 17:05:54 -0300

    style: implement `tailwindcss` in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 function Page() {
-  return <h1>SaaS Boilerplate</h1>;
+  return (
+    <div className="min-h-dvh flex flex-col items-center justify-center">
+      <h1 className="text-3xl">SaaS Boilerplate</h1>
+    </div>
+  );
 }
 
 export default Page;
