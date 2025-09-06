d26947a2afa4af265d18b5edf0e39f28432ac4c5
adspacheco <andersonac7@outlook.com>
2025-09-06 06:29:33 -0300

    feat: integrate database in status endpoint

--- app/api/v1/status/route.ts
+++ app/api/v1/status/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
+import { db } from "../../../../database/client.ts";
+
 
 export async function GET() {
+  const result = await db.execute("SELECT 1+1 as sum;");
+  console.log(result.rows)
   return NextResponse.json({ saas: "boilerplate" }, { status: 200 });
 }
