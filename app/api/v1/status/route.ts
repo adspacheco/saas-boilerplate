f1000867f3d95eb56916e808f42d82b6a1b352f2
adspacheco <andersonac7@outlook.com>
2025-09-06 09:44:49 -0300

    feat: finish `/api/v1/status` endpoint

--- app/api/v1/status/route.ts
+++ app/api/v1/status/route.ts
@@ -1,8 +1,39 @@
 import { NextResponse } from "next/server";
 import { db } from "database/client.ts";
+import { sql } from "drizzle-orm";
 
 export async function GET() {
-  const result = await db.execute("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  return NextResponse.json({ saas: "boilerplate" }, { status: 200 });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await db.execute(sql`SHOW server_version;`);
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await db.execute(
+    sql`SHOW max_connections;`,
+  );
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0]
+    .max_connections as string;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await db.execute(
+    sql`SELECT count(*)::int FROM pg_stat_activity WHERE datname = ${databaseName}`,
+  );
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  return NextResponse.json(
+    {
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: databaseVersionValue,
+          max_connections: parseInt(databaseMaxConnectionsValue, 10),
+          opened_connections: databaseOpenedConnectionsValue,
+        },
+      },
+    },
+    {
+      status: 200,
+    },
+  );
 }
--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -3,9 +3,15 @@ import { describe, expect, it } from "vitest";
 describe("/api/status", () => {
   it("GET to /api/status should return 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
-    const data = await response.json();
-
     expect(response.status).toBe(200);
-    expect(data).toEqual({ saas: "boilerplate" });
+
+    const responseBody = await response.json();
+
+    const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+    expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+    expect(responseBody.dependencies.database.version).toEqual("16.0");
+    expect(responseBody.dependencies.database.max_connections).toEqual(100);
+    expect(responseBody.dependencies.database.opened_connections).toEqual(1);
   });
 });
